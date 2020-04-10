a64a34f4a713a78a6cd1511e045c7c650f8be5e3
Gustav Sterbrant <gustav.sterbrant@gmail.com>
2020-04-10 23:06:43 +0200

    Added animations and images for enerfy and profoto pages.

--- main.js
+++ main.js
@@ -24,91 +24,137 @@ function RemoveGradient(event)
 
 //------------------------------------------
 /**
+   Open the full-page popup
  */
 function OpenPopup(url)
 {
 	var popup = document.getElementById("popup");
 	var container = popup.children[0];
 	popup.classList.add("popup-window-open");
-	setTimeout(function()
-			   {
-				   var xhttp = new XMLHttpRequest();
-				   xhttp.onreadystatechange = function()
-				   {
-					   if (this.readyState == 4 && this.status == 200)
-					   {
-						   container.classList.add("fadein-frame");
-						   container.innerHTML = xhttp.responseText;
-					   }
-				   };
-				   xhttp.open("GET", url);
-				   xhttp.send();
-			   }, 950);
+	setTimeout(
+		function()
+		{
+			var xhttp = new XMLHttpRequest();
+			xhttp.onreadystatechange = function()
+			{
+				if (this.readyState == 4 && this.status == 200)
+				{
+					container.classList.add("fadein-frame");
+					container.innerHTML = xhttp.responseText;
+					pauseGL = true;
+
+					var reactToScrollElements = container.querySelectorAll(".react-on-scroll");
+					Array.prototype.forEach.call(
+						reactToScrollElements,
+						function(element)
+						{
+							// bind the scrolling window, which is the popup container, and add
+							// all the reaction classes automatically
+							element.scrollWindow = container;
+						});
+
+					// find all subpage elements with the class block-big
+					var blockBigElements = container.querySelectorAll('.block-big');
+					Array.prototype.forEach.call(
+						blockBigElements,
+						function(element)
+						{
+							// bind the scrolling window, which is the popup container, and add
+							// all the reaction classes automatically
+							element.scrollWindow = container;
+							element.classList.add("react-on-scroll", "fade-on-scroll", "scale-on-scroll");
+						});
+
+
+					// update elements to notify list
+					elementsToNotifyScroll = document.querySelectorAll('.react-on-scroll');
+				}
+			};
+			xhttp.open("GET", url + "?" + cacheVersionString);
+			xhttp.send();
+		}, 950);
 }
 
 //------------------------------------------
 /**
+   Close full-page popup
  */
 function ClosePopup()
 {
+	// check if the elements implements click, in which case, don't close the popup
+	if (window.event.target.onclick != null)
+	{
+		return;
+	}
 	var popup = document.getElementById("popup");
 	var container = popup.children[0];
 	container.classList.remove("fadein-frame");
 	setTimeout(function()
 			   {
+				   container.scrollTop = 0;
 				   container.removeChild(container.children[0]);
 				   popup.classList.remove("popup-window-open");
+				   pauseGL = false;
 			   }, 500);
 }
 
 //------------------------------------------
 /**
-*/
-function IsElementInViewport (el)
+   Avoids closing the popup and instead just fades out, loads new content, and fades in again
+ */
+function ReopenPopup(newUrl)
 {
-    var rect = el.getBoundingClientRect();
+	var popup = document.getElementById("popup");
+	var container = popup.children[0];
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function()
+	{
+		if (this.readyState == 4 && this.status == 200)
+		{
+			container.classList.remove("fadein-frame");
+			setTimeout(function()
+					   {
+						   container.scrollTop = 0;
+						   container.classList.add("fadein-frame");
+						   container.innerHTML = xhttp.responseText;
 
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+						   // find all subpage elements with the class block-big
+						   var subpageElements = container.querySelectorAll('.block-big');
+						   Array.prototype.forEach.call(
+							   subpageElements,
+							   function(element)
+							   {
+								   // bind the scrolling window, which is the popup container, and add
+								   // all the reaction classes automatically
+								   element.scrollWindow = container;
+								   element.classList.add("react-on-scroll", "fade-on-scroll", "scale-on-scroll");
+							   });
+
+						   // update elements to notify list
+						   elementsToNotifyScroll = document.querySelectorAll('.react-on-scroll');
+					   }, 500);
+		}
+	};
+	xhttp.open("GET", newUrl + "?" + cacheVersionString);
+	xhttp.send();
 }
 
 //------------------------------------------
 /**
 */
-function IsElementClippedInViewport (el)
+function OnScrollIn(element, callback)
 {
-    var rect = el.getBoundingClientRect();
-
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.left <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
-
-//------------------------------------------
-/**
-*/
-function OnScrollIn(element, callback, divisor)
-{
-	// a small hack, but since we might rescale this element, we should remember the original position
-	if (element.origTop == undefined)
-		element.origTop = element.offsetTop;
 
 	// get the viewport bottom value
-	var scrollPos = window.pageYOffset + window.innerHeight;
-	var elementPos = element.origTop;
+	var scrollPos = element.scrollWindow.scrollTop + element.scrollWindow.clientHeight;
+	var elementPos = element.offsetTop;
 
 	// if the viewport bottom is below the top of the element, start the effect
 	if (elementPos < scrollPos)
 	{
 		// calculate the factor, the divisor will tell how 'fast' the animation should happen
-		var factor = (scrollPos - elementPos) / divisor;
+		let style = getComputedStyle(element);
+		var factor = (scrollPos - elementPos) / (element.offsetHeight * style.getPropertyValue("--scroll-multiplier"));
 		factor = Math.min(Math.max(factor, 0), 1);
 
 		// run the callback and pass the factor
@@ -116,6 +162,7 @@ function OnScrollIn(element, callback, divisor)
 	}
 }
 
+var elementsToNotifyScroll = [];
 //------------------------------------------
 /**
 */
@@ -127,8 +174,13 @@ window.onload = function()
 	{
 		window.setTimeout(callback, 1000/60);
 	};
-	var elementsToNotifyScroll = document.querySelectorAll('.react-on-scroll');
-
+	elementsToNotifyScroll = document.querySelectorAll('.react-on-scroll');
+	Array.prototype.forEach.call(
+		elementsToNotifyScroll,
+		function(element)
+		{
+			element.scrollWindow = document.documentElement;
+		});
 	// connect to websocket
 	/*
 	const socket = new WebSocket("ws://www.pustoshi.com:8080/fetch");
@@ -155,11 +207,12 @@ window.onload = function()
 			elementsToNotifyScroll,
 			function(element)
 			{
-				OnScrollIn(element,
-						   function(factor, element)
-						   {
-							   element.style.setProperty("--scroll-factor", factor);
-						   }, 500);
+				OnScrollIn(
+					element,
+					function(factor, element)
+					{
+						element.style.setProperty("--scroll-factor", factor);
+					});
 			}
 		);
 
